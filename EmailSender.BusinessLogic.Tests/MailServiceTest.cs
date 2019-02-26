6a5b010545e116aeb79aea5412348c1b370228e0
Vinod Malvi <vinod.malvi@yahoo.co.uk>
2019-02-26 22:32:28 +0000

    Added unit test project

---
+++ EmailSender.BusinessLogic.Tests/MailServiceTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EmailSender.BusinessLogic.Tests
+{
+    [TestClass]
+    public class MailServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
